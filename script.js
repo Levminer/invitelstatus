1a41a3010313adb842a115d2bc5a04421fe15dfb
Levminer <33373714+Levminer@users.noreply.github.com>
2020-01-29 16:28:39 +0100

    IS V3

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//counter
 function counter() {
     let counter = document.getElementById("counter").innerHTML = "4"
+}
+
+//history
+function history() {
+    alert("Leállások:\n 01.08 \n 01.09 \n 01.20 (2)")
 }
