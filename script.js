4d32824ff1e96e84f10a5c51b906e8672001e564
Levminer <33373714+Levminer@users.noreply.github.com>
2020-02-17 15:23:53 +0100

    IS V3.3

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //counter
 function counter() {
-    let counter = document.getElementById("counter").innerHTML = "6" // modify
+    let counter = document.getElementById("counter").innerHTML = "7" // modify
 }
 
 //history
 function history() {
-    alert("Leállások:\n 01.08 \n 01.09 \n 01.20 (2) \n 02.05 \n 02.12") // modify
+    alert("Leállások:\n 01.08 \n 01.09 \n 01.20 (2) \n 02.05 \n 02.12 \n 02.17") // modify
 }
