351d7ab6ca99c217d2c8337dfac544f385b1e67b
Levminer <33373714+Levminer@users.noreply.github.com>
2020-03-31 16:16:29 +0200

    IS V4

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+//onload
+$(document).ready(function() {
+	counter()
+	date()
+})
+
 //counter
 function counter() {
-	let counter = (document.getElementById("counter").innerHTML = "10") // modify
+	let counter = (document.getElementById("counter").innerHTML = "12") // modify
+}
+
+//date
+function date() {
+	let date = (document.getElementById("date").innerHTML = "Utolsó hiba: 2020.03.31.") // modify
 }
 
 //history
 function history() {
-	alert("Leállások:\n 01.08 \n 01.09 \n 01.20 (2) \n 02.05 \n 02.12 \n 02.17 \n 02.19 \n 02.20 \n 03.24") // modify
+	alert("Leállások:\n 01.08 \n 01.09 \n 01.20 (2) \n 02.05 \n 02.12 \n 02.17 \n 02.19 \n 02.20 \n 03.24 \n 3.31 (2)") // modify
 }
