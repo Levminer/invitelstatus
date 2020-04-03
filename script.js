a2345916a2921e746db3f106d3c77f77094cb04c
Levminer <33373714+Levminer@users.noreply.github.com>
2020-04-03 14:40:14 +0200

    IS V4.1

--- script.js
+++ script.js
@@ -16,5 +16,5 @@ function date() {
 
 //history
 function history() {
-	alert("Leállások:\n 01.08 \n 01.09 \n 01.20 (2) \n 02.05 \n 02.12 \n 02.17 \n 02.19 \n 02.20 \n 03.24 \n 3.31 (2)") // modify
+	alert("Leállások:\n 01.08 \n 01.09 \n 01.20 (2) \n 02.05 \n 02.12 \n 02.17 \n 02.19 \n 02.20 \n 03.24 \n 03.31 (2)") // modify
 }
