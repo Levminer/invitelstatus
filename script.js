94dcfbb477692038e4db00296e8b820fe3ff0751
Levminer <33373714+Levminer@users.noreply.github.com>
2020-04-09 12:35:55 +0200

    IS V4.2
    New date

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 //onload
-$(document).ready(function() {
+$(document).ready(function () {
 	counter()
 	date()
 })
 
 //counter
 function counter() {
-	let counter = (document.getElementById("counter").innerHTML = "12") // modify
+	let counter = (document.getElementById("counter").innerHTML = "13") // modify
 }
 
 //date
 function date() {
-	let date = (document.getElementById("date").innerHTML = "Utolsó hiba: 2020.03.31.") // modify
+	let date = (document.getElementById("date").innerHTML = "Utolsó hiba: 2020.04.09.") // modify
 }
 
 //history
 function history() {
-	alert("Leállások:\n 01.08 \n 01.09 \n 01.20 (2) \n 02.05 \n 02.12 \n 02.17 \n 02.19 \n 02.20 \n 03.24 \n 03.31 (2)") // modify
+	alert("Leállások:\n 01.08 \n 01.09 \n 01.20 (2) \n 02.05 \n 02.12 \n 02.17 \n 02.19 \n 02.20 \n 03.24 \n 03.31 (2) \n 04.09") // modify
 }
