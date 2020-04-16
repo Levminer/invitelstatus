03ce5a346aef3ea9ae05dba5e4e8a2d6e73d90a8
Levminer <33373714+Levminer@users.noreply.github.com>
2020-04-16 12:29:35 +0200

    IS V4.5

--- assets/script.js
+++ assets/script.js
@@ -6,17 +6,17 @@ $(document).ready(function () {
 
 //counter
 function counter() {
-	let counter = (document.getElementById("counter").innerHTML = "15") // modify
+	let counter = (document.getElementById("counter").innerHTML = "16") // modify
 }
 
 //date
 function date() {
-	let date = (document.getElementById("date").innerHTML = "Utolsó hiba: 2020.04.15.") // modify
+	let date = (document.getElementById("date").innerHTML = "Utolsó hiba: 2020.04.16.") // modify
 }
 
 //history
 function history() {
 	alert(
-		"Leállások:\n 01.08 \n 01.09 \n 01.20 (2) \n 02.05 \n 02.12 \n 02.17 \n 02.19 \n 02.20 \n 03.24 \n 03.31 (2) \n 04.09 \n 04.14 \n 04.15"
+		"Leállások:\n 01.08 \n 01.09 \n 01.20 (2) \n 02.05 \n 02.12 \n 02.17 \n 02.19 \n 02.20 \n 03.24 \n 03.31 (2) \n 04.09 \n 04.14 \n 04.15 \n 04.16"
 	) // modify
 }
