1bd90063fc2ef32198a0302c3921704183345737
Levminer <33373714+Levminer@users.noreply.github.com>
2020-05-14 20:05:53 +0200

    IS V4.11

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
 //counter
 function counter() {
-	let counter = (document.getElementById("counter").innerHTML = "21") // modify
+	let counter = (document.getElementById("counter").innerHTML = "23") // modify
 }
 
 //date
@@ -17,6 +17,6 @@ function date() {
 //history
 function history() {
 	alert(
-		"Leállások:\n 01.08 \n 01.09 \n 01.20 \n 01.20 \n 02.05 \n 02.12 \n 02.17 \n 02.19 \n 02.20 \n 03.24 \n 03.31 \n 03.31 \n 04.09 \n 04.14 \n 04.15 \n 04.16 \n 05.01 (kb. 3 óra) \n 05.01 (kb. 4 perc) \n 05.10 (kb. 20 perc) \n 05.12 (kb. 10 perc) \n 05.14 (kb. 10 perc)"
+		"Leállások:\n 01.08 \n 01.09 \n 01.20 \n 01.20 \n 02.05 \n 02.12 \n 02.17 \n 02.19 \n 02.20 \n 03.24 \n 03.31 \n 03.31 \n 04.09 \n 04.14 \n 04.15 \n 04.16 \n 05.01 (kb. 3 óra) \n 05.01 (kb. 4 perc) \n 05.10 (kb. 20 perc) \n 05.12 (kb. 10 perc) \n 05.14 (kb. 10 perc) \n 05.14 (kb. 5 perc) \n 05.14 (kb. 15 perc)"
 	) // modify
 }
