b08e5ba6b25051d683a997fd6ea10e8f45f202e9
Levminer <33373714+Levminer@users.noreply.github.com>
2020-05-19 14:42:39 +0200

    IS V5
    -Rewritten in TS

--- assets/script.ts
+++ assets/script.ts
@@ -1,32 +1,41 @@
-"use strict"
+declare const jquery: any
+declare const $: any
+
 //?onload
 $(document).ready(function () {
 	counter()
 	date()
 })
+
 //?counter
 let counter = () => {
-	let counter = document.getElementById("counter")
+	let counter: any = <HTMLInputElement>document.getElementById("counter")
 	counter.innerHTML = "31" //modify "XX"
 }
+
 //?date
 let date = () => {
-	let date = document.getElementById("date")
+	let date: any = <HTMLInputElement>document.getElementById("date")
 	date.innerHTML = "Utolsó hiba: 2020.05.18."
 }
+
 //?history
 let history_2020_01 = () => {
 	alert("Leállások (4): \n 01.08 \n 01.09 \n 01.20 \n 01.20")
 }
+
 let history_2020_02 = () => {
 	alert("Leállások (5): \n 02.05 \n 02.12 \n 02.17 \n 02.19 \n 02.20 ")
 }
+
 let history_2020_03 = () => {
 	alert("Leállások (3): \n 03.24 \n 03.31 \n 03.31")
 }
+
 let history_2020_04 = () => {
 	alert("Leállások (4): \n 04.09 \n 04.14 \n 04.15 \n 04.16")
 }
+
 let history_2020_05 = () => {
 	alert(
 		//modify "Leállások (X)"
